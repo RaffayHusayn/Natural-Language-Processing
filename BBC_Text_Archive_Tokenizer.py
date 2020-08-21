5e88fdf1ed91fd603428fb22993e9067828cb142
raffayhusayn <raffayhusayn@gmail.com>
2020-08-21 23:27:47 +0500

    Comments added for future understanding

--- BBC_Text_Archive_Tokenizer.py
+++ BBC_Text_Archive_Tokenizer.py
@@ -1,3 +1,4 @@
+#download the BBC Text Archive dataset and storig it in tmp/bbc_text.csv (Works on Google Colab)
 !wget --no-check-certificate \
     https://storage.googleapis.com/laurencemoroney-blog.appspot.com/bbc-text.csv \
     -O /tmp/bbc-text.csv
@@ -10,10 +11,11 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 #we can obviously use NLTK's stopword from "from nltk.corpus import stopwords" as well but this works quite well
 def stopword():
 	return [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ];
-
 stopwords = stopword()
+
 sentences = []
 labels = []
+#importing the csv and saving the content into lists
 with open("/tmp/bbc-text.csv", 'r') as csvfile:
       datastore = csv.reader(csvfile)
 
@@ -22,21 +24,25 @@ with open("/tmp/bbc-text.csv", 'r') as csvfile:
             sentences.append(item[1])
             labels.append(item[0])
 
+#removing the labels from the first index
 sentences = sentences[1:]
 labels = labels[1:]
+
 print(len(sentences))
 print(sentences[0])
 print(len(labels))
 print(labels[0])
 
+#Removing Stopwords from the sentences
 filtered_sentences = []
-print(stopwords)
 
 for sentence in sentences:
   sentence_split= sentence.split()
   ind_sentence = [word for word in sentence_split if word not in stopwords]
   filtered_sentences.append(ind_sentence)
 
+
+#Tokenizing the Sentences
 tokenizer = Tokenizer(oov_token ='<OOV>')
 tokenizer.fit_on_texts(filtered_sentences)
 word_index = tokenizer.word_index
@@ -51,6 +57,7 @@ padded = pad_sequences(sequences, padding='post')
 print(padded[0])
 print(padded.shape)
 
+#Tokenizing the Labels
 label_tokenizer = Tokenizer(oov_token ='<OOV>')
 label_tokenizer.fit_on_texts(labels)
 label_word_index = label_tokenizer.word_index
