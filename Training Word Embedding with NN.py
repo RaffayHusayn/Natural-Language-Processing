13f0180e696a9e6f86fa429cb908648e83adad08
raffayhusayn <raffayhusayn@gmail.com>
2020-09-02 02:43:04 +0500

    Week 2, Lecture 1 of NLP course

---
+++ Training Word Embedding with NN.py
@@ -0,0 +1,115 @@
+import tensorflow as tf
+print(tf.__version__)
+# !pip install -q tensorflow-datasets
+
+import tensorflow_datasets as tfds
+imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+
+import numpy as np
+
+train_data, test_data = imdb['train'], imdb['test']
+
+training_sentences = []
+training_labels = []
+
+testing_sentences = []
+testing_labels = []
+
+# str(s.tonumpy()) is needed in Python3 instead of just s.numpy()
+for s,l in train_data:
+  training_sentences.append(s.numpy().decode('utf8'))
+  training_labels.append(l.numpy())
+
+for s,l in test_data:
+  testing_sentences.append(s.numpy().decode('utf8'))
+  testing_labels.append(l.numpy())
+
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
+
+import numpy as np
+
+train_data, test_data = imdb['train'], imdb['test']
+
+training_sentences = []
+training_labels = []
+
+testing_sentences = []
+testing_labels = []
+
+# str(s.tonumpy()) is needed in Python3 instead of just s.numpy()
+for s,l in train_data:
+  training_sentences.append(s.numpy().decode('utf8'))
+  training_labels.append(l.numpy())
+
+for s,l in test_data:
+  testing_sentences.append(s.numpy().decode('utf8'))
+  testing_labels.append(l.numpy())
+
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
+
+import numpy as np
+
+train_data, test_data = imdb['train'], imdb['test']
+
+training_sentences = []
+training_labels = []
+
+testing_sentences = []
+testing_labels = []
+
+# str(s.tonumpy()) is needed in Python3 instead of just s.numpy()
+for s,l in train_data:
+  training_sentences.append(s.numpy().decode('utf8'))
+  training_labels.append(l.numpy())
+
+for s,l in test_data:
+  testing_sentences.append(s.numpy().decode('utf8'))
+  testing_labels.append(l.numpy())
+
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(6, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.summary()
+num_epochs = 10
+model.fit(padded, training_labels_final, epochs=num_epochs, validation_data=(testing_padded, testing_labels_final))
+
+e = model.layers[0]
+weights = e.get_weights()[0]
+print(weights.shape) # shape: (vocab_size, embedding_dim)
+
+import io
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(1, vocab_size):
+  word = reverse_word_index[word_num]
+  embeddings = weights[word_num]
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
+
+import io
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(1, vocab_size):
+  word = reverse_word_index[word_num]
+  embeddings = weights[word_num]
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
+
+sentence = "I really think this is amazing. honest."
+sequence = tokenizer.texts_to_sequences([sentence])
+print(sequence)
